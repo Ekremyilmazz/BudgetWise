b6778a0560862243e8bfb3ed7e9724ed9ba9e103
Ekremyilmazz <ekremyilmaz4217@gmail.com>
2025-07-13 11:26:24 +0300

    pie chart is setted up to responsive design

--- src/components/PieChart.tsx
+++ src/components/PieChart.tsx
@@ -1,12 +1,31 @@
 
 import { PieChart, Pie, Cell, Tooltip, Legend, ResponsiveContainer } from "recharts";
 import { useAppSelector } from "../app/hooks";
+import { useState, useEffect } from "react";
 
 // Colors for categories
 const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#AA00FF"];
 
 const CustomPieChart = () => {
   const transactions = useAppSelector((state) => state.budget.transactions);
+
+  const [outerRadius, setOuterRadius] = useState(80);
+
+  useEffect(() => {
+    const handleSize = () => {
+      const width = window.innerWidth;
+      if(width <= 375) {
+        setOuterRadius(40);
+      } else if (width <= 430) {
+        setOuterRadius(50);
+      } else {
+        setOuterRadius(80);
+      }
+    };
+
+    handleSize();
+    window.addEventListener("resize", handleSize);
+  }, [])
 
   // Count the total amount based on categories
   const categoryTotals: { [key: string]: number } = {};
@@ -33,7 +52,7 @@ const CustomPieChart = () => {
             data={data}
             cx="50%"
             cy="50%"
-            outerRadius={80}
+            outerRadius={outerRadius}
             fill="#8884d8"
             label={({ name, value }) => `${name}: $${value}`}
           >
